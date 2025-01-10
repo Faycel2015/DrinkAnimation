51552ad7a15d17f7101a680c022f293428d0603c
Faycel2015 <faycel.ayech@gmail.com>
2025-01-11 00:31:07 +0100

    Initial commit with Xcode project

---
+++ DrinkAnimation/DrinkAnimation/DrinkAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkAnimationApp.swift
+//  DrinkAnimation
+//
+//  Created by FayTek on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct DrinkAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
